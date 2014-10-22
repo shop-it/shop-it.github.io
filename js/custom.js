352eabbfa3e9f50cdf6695fa3b78ed96327ce5ae
trackem <apptrackem@gmail.com>
2014-10-22 02:06:40 +0100

    Trackem website

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+	$(".navbar-nav li a").click(function(event) {
+		$(".navbar-collapse").collapse('hide');
+	});
+	
+	$("#scroll").on('click', function(e) {
+	   e.preventDefault();
+	   var hash = this.hash;
+	   $('html, body').animate({
+		   scrollTop: $(this.hash).offset().top
+		}, 500, function(){
+		   window.location.hash = hash;
+		});
+	});
+	
+	$("a[href^='#']").on('click', function(e) {
+		e.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 500, function(){
+			window.location.hash = hash;
+		});
+	});
+});
+
