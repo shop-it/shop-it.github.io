5d35a8ac45dfe9f17b1c4af25f0359cc4d4fd82b
Marcus Gomes <mvp.gomes1989@gmail.com>
2014-11-30 12:46:28 +0000

    Add the background carousel.

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 	$(".navbar-nav li a").click(function(event) {
 		$(".navbar-collapse").collapse('hide');
 	});
-	
+
 	$("#scroll").on('click', function(e) {
 	   e.preventDefault();
 	   var hash = this.hash;
@@ -12,7 +12,7 @@ $(document).ready(function () {
 		   window.location.hash = hash;
 		});
 	});
-	
+
 	$("a[href^='#']").on('click', function(e) {
 		e.preventDefault();
 		var hash = this.hash;
@@ -22,5 +22,8 @@ $(document).ready(function () {
 			window.location.hash = hash;
 		});
 	});
-});
 
+	$('.carousel').carousel(
+		interval: 7000
+	});
+});
