b75b731a692f74c1fa3a42cf7f7651aa56eaf645
Marcus Gomes <mvp.gomes1989@gmail.com>
2014-12-01 12:24:37 +0000

    Update the layout.

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 	$(".navbar-nav li a").click(function(event) {
 		$(".navbar-collapse").collapse('hide');
 	});
-	
+
 	$("#scroll").on('click', function(e) {
 	   e.preventDefault();
 	   var hash = this.hash;
@@ -12,7 +12,15 @@ $(document).ready(function () {
 		   window.location.hash = hash;
 		});
 	});
-	
+
+	$(document).on('scroll', function(){
+		if($(document).scrollTop()>450){
+			$("#logo-description").addClass("scrolled");
+		} else{
+			$("#logo-description").removeClass("scrolled");
+		}
+	});
+
 	$("a[href^='#']").on('click', function(e) {
 		e.preventDefault();
 		var hash = this.hash;
@@ -23,4 +31,3 @@ $(document).ready(function () {
 		});
 	});
 });
-
